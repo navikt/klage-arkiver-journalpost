76b9cbbc96b91757129371b0422ef2c1b39f1a9c
Andreas Jonsson <andreas.jonsson@nav.no>
2020-08-14 16:17:02 +0200

    Some formatting.

--- src/main/kotlin/no/nav/klage/domain/Klage.kt
+++ src/main/kotlin/no/nav/klage/domain/Klage.kt
@@ -3,24 +3,24 @@ package no.nav.klage.domain
 import java.time.LocalDate
 
 data class Klage(
-        val id: Int,
-        val identifikasjonstype: String,
-        val identifikasjonsnummer: String,
-        val klageInstans: Boolean,
-        val trygderetten: Boolean,
-        val fornavn: String,
-        val mellomnavn: String,
-        val etternavn: String,
-        val adresse: String,
-        val telefon: String,
-        val vedtak: String,
-        val saksnummer: String?,
-        val dato: LocalDate,
-        val begrunnelse: String,
-        val tema: String,
-        val ytelse: String,
-        val vedlegg: List<Vedlegg> = emptyList(),
-        var fileContentAsBytes: ByteArray? = null
+    val id: Int,
+    val identifikasjonstype: String,
+    val identifikasjonsnummer: String,
+    val klageInstans: Boolean,
+    val trygderetten: Boolean,
+    val fornavn: String,
+    val mellomnavn: String,
+    val etternavn: String,
+    val adresse: String,
+    val telefon: String,
+    val vedtak: String,
+    val saksnummer: String?,
+    val dato: LocalDate,
+    val begrunnelse: String,
+    val tema: String,
+    val ytelse: String,
+    val vedlegg: List<Vedlegg> = emptyList(),
+    var fileContentAsBytes: ByteArray? = null
 )
 
 data class Vedlegg(
--- src/main/kotlin/no/nav/klage/domain/KlagePDFModel.kt
+++ src/main/kotlin/no/nav/klage/domain/KlagePDFModel.kt
@@ -1,16 +1,16 @@
 package no.nav.klage.domain
 
 data class KlagePDFModel(
-        val foedselsnummer: String,
-        val fornavn: String,
-        val mellomnavn: String,
-        val etternavn: String,
-        val adresse: String,
-        val telefonnummer: String,
-        val vedtak: String,
-        val begrunnelse: String,
-        val saksnummer: String,
-        val oversiktVedlegg: String,
-        val dato: String,
-        val ytelse: String
+    val foedselsnummer: String,
+    val fornavn: String,
+    val mellomnavn: String,
+    val etternavn: String,
+    val adresse: String,
+    val telefonnummer: String,
+    val vedtak: String,
+    val begrunnelse: String,
+    val saksnummer: String,
+    val oversiktVedlegg: String,
+    val dato: String,
+    val ytelse: String
 )
