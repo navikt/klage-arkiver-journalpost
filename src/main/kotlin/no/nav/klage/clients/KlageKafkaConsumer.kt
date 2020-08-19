5decc7859799335a0014107ffe3742d4d380011f
Richard Borge <richard.borge@nav.no>
2020-08-19 14:21:55 +0200

    Run catching so we dont inadvertently log personal data

--- src/main/kotlin/no/nav/klage/clients/KlageKafkaConsumer.kt
+++ src/main/kotlin/no/nav/klage/clients/KlageKafkaConsumer.kt
@@ -24,19 +24,17 @@ class KlageKafkaConsumer(private val applicationService: ApplicationService) {
     fun listen(klageRecord: ConsumerRecord<String, String>) {
         logger.debug("Klage received from Kafka topic")
         secureLogger.debug("Klage received from Kafka topic: {}", klageRecord.value())
-        val klage = klageRecord.value().toKlage()
-        logger.debug("Received klage has id: {}", klage.id)
-        secureLogger.debug("Received klage has id: {} and fnr: {}", klage.id, klage.identifikasjonsnummer)
 
-        applicationService.createJournalpost(klage)
-    }
-
-    private fun String.toKlage(): Klage {
-        return try {
-            mapper.readValue(this, Klage::class.java)
-        } catch (e: Exception) {
-            secureLogger.error("Could not read/parse JSON value into klage object: $this", e)
-            throw e
+        runCatching {
+            val klage = klageRecord.value().toKlage()
+            logger.debug("Received klage has id: {}", klage.id)
+            secureLogger.debug("Received klage has id: {} and fnr: {}", klage.id, klage.identifikasjonsnummer)
+            applicationService.createJournalpost(klage)
+        }.onFailure {
+            secureLogger.error("Failed to process klage", it)
         }
     }
+
+    private fun String.toKlage(): Klage = mapper.readValue(this, Klage::class.java)
+
 }
