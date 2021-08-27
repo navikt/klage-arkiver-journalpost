f3cefc82d1cf5c14156184e360bbcfd3838d1d50
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-08-27 13:37:32 +0200

    Allows ModelParsingException. PDF files are valiated in klage-dittnav-api on upload, should not fail here.

--- src/main/kotlin/no/nav/klage/service/PdfService.kt
+++ src/main/kotlin/no/nav/klage/service/PdfService.kt
@@ -1,20 +1,32 @@
 package no.nav.klage.service
 
+import no.nav.klage.getLogger
 import org.springframework.stereotype.Service
+import org.verapdf.core.ModelParsingException
 import org.verapdf.pdfa.Foundries
 import org.verapdf.pdfa.VeraGreenfieldFoundryProvider
 import java.io.ByteArrayInputStream
 
 @Service
 class PdfService {
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
     init {
         VeraGreenfieldFoundryProvider.initialise()
     }
 
     fun pdfByteArrayIsPdfa(byteArray: ByteArray): Boolean {
-        val parser = Foundries.defaultInstance().createParser(ByteArrayInputStream(byteArray))
-        val validator = Foundries.defaultInstance().createValidator(parser.flavour, false)
-        val result = validator.validate(parser)
-        return result.isCompliant
+        try {
+            val parser = Foundries.defaultInstance().createParser(ByteArrayInputStream(byteArray))
+            val validator = Foundries.defaultInstance().createValidator(parser.flavour, false)
+            val result = validator.validate(parser)
+            return result.isCompliant
+        } catch (e: ModelParsingException) {
+            logger.warn("Error parsing document: {}", e)
+        }
+        return false
     }
 }
