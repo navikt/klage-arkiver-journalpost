c6ed6def56dc0ae719b483bccf18f2c67fa8db60
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2022-11-18 15:23:43 +0100

    Dependency fix.

--- src/main/kotlin/no/nav/klage/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/SecurityConfiguration.kt
@@ -1,8 +1,10 @@
 package no.nav.klage.config
 
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
 
+@EnableJwtTokenValidation(ignore = ["org.springdoc"])
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
-internal class SecurityConfiguration 
+internal class SecurityConfiguration
