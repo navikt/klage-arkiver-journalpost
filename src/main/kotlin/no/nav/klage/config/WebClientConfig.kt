312fcfd2657b27258a4ad726eb57c12091195975
Andreas Jonsson <andreas.jonsson@nav.no>
2025-09-15 11:08:29 +0200

    Reverting until we know what the error is. Many incoming klanker fail.

--- src/main/kotlin/no/nav/klage/config/WebClientConfig.kt
+++ src/main/kotlin/no/nav/klage/config/WebClientConfig.kt
@@ -14,23 +14,23 @@ import java.util.concurrent.TimeUnit
 @Configuration
 class WebClientConfig {
 
-    @Bean
-    fun reactorNettyHttpClient(): HttpClient {
-        val timeoutInSeconds = 100L
-        return HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000)
-            .responseTimeout(Duration.ofSeconds(timeoutInSeconds))
-            .doOnConnected { conn ->
-                conn.addHandlerLast(ReadTimeoutHandler(timeoutInSeconds, TimeUnit.SECONDS))
-                conn.addHandlerLast(WriteTimeoutHandler(timeoutInSeconds, TimeUnit.SECONDS))
-            }
-    }
-
-    @Bean
-    fun webClientBuilder(httpClient: HttpClient): WebClient.Builder {
-        val connector = ReactorClientHttpConnector(httpClient)
-        return WebClient.builder()
-            .clientConnector(connector)
-    }
+//    @Bean
+//    fun reactorNettyHttpClient(): HttpClient {
+//        val timeoutInSeconds = 100L
+//        return HttpClient.create()
+//            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000)
+//            .responseTimeout(Duration.ofSeconds(timeoutInSeconds))
+//            .doOnConnected { conn ->
+//                conn.addHandlerLast(ReadTimeoutHandler(timeoutInSeconds, TimeUnit.SECONDS))
+//                conn.addHandlerLast(WriteTimeoutHandler(timeoutInSeconds, TimeUnit.SECONDS))
+//            }
+//    }
+//
+//    @Bean
+//    fun webClientBuilder(httpClient: HttpClient): WebClient.Builder {
+//        val connector = ReactorClientHttpConnector(httpClient)
+//        return WebClient.builder()
+//            .clientConnector(connector)
+//    }
 
 }
