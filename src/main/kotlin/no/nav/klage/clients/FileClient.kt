b5d470ef15f6f39bb817abe7cfc494875b6661b0
Andreas Jonsson <andreas.jonsson@nav.no>
2025-10-08 09:40:27 +0200

    Retryable file client.

--- src/main/kotlin/no/nav/klage/clients/FileClient.kt
+++ src/main/kotlin/no/nav/klage/clients/FileClient.kt
@@ -3,6 +3,7 @@ package no.nav.klage.clients
 import no.nav.klage.util.TokenUtil
 import no.nav.klage.util.getLogger
 import org.springframework.http.HttpHeaders
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
@@ -18,6 +19,7 @@ class FileClient(
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
+    @Retryable
     fun getAttachment(id: String): ByteArray {
         logger.debug("Fetching attachment with id {}", id)
 
@@ -29,6 +31,7 @@ class FileClient(
             .block() ?: throw RuntimeException("Attachment could not be fetched")
     }
 
+    @Retryable
     fun deleteAttachment(id: String) {
         logger.debug("Deleting attachment with id {}", id)
 
