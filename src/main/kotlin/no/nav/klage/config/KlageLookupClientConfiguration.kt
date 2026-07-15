9038ab99dbc93cf3a46f4dfdbf07dfd8f76ea5ec
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-07-15 14:37:32 +0200

    Adjusted web client setup.

--- src/main/kotlin/no/nav/klage/config/KlageLookupClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/KlageLookupClientConfiguration.kt
@@ -1,19 +1,22 @@
 package no.nav.klage.config
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class KlageLookupClientConfiguration(private val webClientBuilder: WebClient.Builder) {
+class KlageLookupClientConfiguration(
+    @Qualifier("standardWebClientBuilder")  private val standardWebClientBuilder: WebClient.Builder
+) {
 
     @Value($$"${KLAGE_LOOKUP_BASE_URL}")
     private lateinit var klageLookupUrl: String
 
     @Bean
     fun klageLookupWebClient(): WebClient {
-        return webClientBuilder
+        return standardWebClientBuilder
             .baseUrl(klageLookupUrl)
             .build()
     }
