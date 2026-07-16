7cb4d5226c2487bb0ead0e6f699a6678c829d52e
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-07-16 09:57:24 +0200

    Adjust request class.

--- src/main/kotlin/no/nav/klage/clients/klagelookup/KlageLookupRequest.kt
+++ src/main/kotlin/no/nav/klage/clients/klagelookup/KlageLookupRequest.kt
@@ -1,14 +1,5 @@
 package no.nav.klage.clients.klagelookup
 
-import no.nav.klage.kodeverk.Fagsystem
-import no.nav.klage.kodeverk.ytelse.Ytelse
-
 data class GetPersonRequest(
     val fnr: String,
-)
-
-data class Sak(
-    val sakId: String,
-    val ytelse: Ytelse,
-    val fagsystem: Fagsystem,
 )
